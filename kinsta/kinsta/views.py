341ab4a84b05d0561bb93b1d515a6b15ca42002b
ksm0076 <60387895+ksm0076@users.noreply.github.com>
2024-08-11 22:28:52 +0900

    membership.html 추가

--- kinsta/kinsta/views.py
+++ kinsta/kinsta/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 def index(request):
     return render(request, 'index.html')
 
+def membership(request):
+    return render(request, 'membership.html')
 
 # class sub(APIView):
 #     def get(self, request):
